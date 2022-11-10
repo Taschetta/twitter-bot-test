76751b102ab5b75668dcb475cb55c51176968cf7
= <fernanez.agustina.ailen@gmail.com>
2022-11-10 11:07:45 -0300

    rebuild

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-import { ETwitterStreamEvent, TwitterApi } from "twitter-api-v2";
+import { TwitterApi } from "twitter-api-v2";
 
 const API_KEY = process.env.API_KEY
 const API_KEY_SECRET = process.env.API_KEY_SECRET
-const BEARER_TOKEN = process.env.BEARER_TOKEN
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET
 const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
 const USER_ID = process.env.USER_ID
-const STREAM_RULE_ID = process.env.STREAM_RULE_ID
+const INTERVAL_MS = parseInt(process.env.INTERVAL_MS)
 
-const twitter_write = (new TwitterApi({
+const twitter = (new TwitterApi({
   appKey: API_KEY,
   appSecret: API_KEY_SECRET,
   accessToken: ACCESS_TOKEN,
@@ -19,31 +18,14 @@ const twitter_write = (new TwitterApi({
   clientSecret: CLIENT_SECRET,
 })).readWrite
 
-const twitter_read = (new TwitterApi(BEARER_TOKEN)).readWrite
 
-// UNO
+setInterval(async () => {
+  const start_time = new Date(Date.now() - INTERVAL_MS).toISOString()
+  const tweets = await twitter.v2.search('#visitelabiblioameghino', { start_time })
+ 
+  for (const tweet of tweets) {
+    console.log(tweet)
+    twitter.v2.retweet(USER_ID, tweet.data.id)
+  }
 
-// console.log(await twitter.v2.updateStreamRules({ delete: { ids: [STREAM_RULE_ID] } }))
-// console.log(await twitter_read.v2.updateStreamRules({ add: [{ value: '#visitelabiblioameghino' }] }))
-
-// DOS
-
-const stream = await twitter_read.v2.searchStream({ autoConnect: true })
-
-stream.on(ETwitterStreamEvent.Connected, () => console.log('Stream is started.'));
-
-stream.on(ETwitterStreamEvent.Data, (tweet) => {
-  console.log(tweet)
-  twitter_write.v2.retweet(USER_ID, tweet.data.id)
-    .then(console.log)
-    .catch(console.log)
-});
-
-stream.on(ETwitterStreamEvent.ConnectionError, console.log)
-
-try {
-  await stream.connect()  
-} catch (error) {
-  console.log(error)
-  await stream.close()
-}
+}, INTERVAL_MS);
